bb869c6de31dbada158a53ca75f9358b62bed410
daviPeter07 <davipetersondev173@gmail.com>
2026-06-27 23:14:40 -0400

    feat(auth): add auth schemas, types, and constants

---
+++ frontend/src/modules/auth/auth.constants.ts
@@ -0,0 +1,4 @@
+export const AUTH_COOKIE_NAME = "newsletter_access_token";
+export const AUTH_LOGIN_PATH = "/";
+export const AUTH_REGISTER_PATH = "/register";
+export const AUTH_REDIRECT_PATH = "/preferences";
